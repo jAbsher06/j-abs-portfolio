5738e56d2751a7993aa801a9803eb985c3f60ed5
Jared Absher <jared.absher@gmail.com>
2021-08-22 13:00:05 -0400

    wrapped app in light/dark theme provider

---
+++ components/ContainerBlock.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+export default function ContainerBlock ({ children, ...customMeta }) {
+  const router = useRouter();
+
+  const meta = {
+    title: "Jared Absher -  Developer, Designer, and Engineer",
+    description: "I develop websites that make a positive impact in people's lives. Get in touch with me",
+    image: "",
+    type: "website",
+    ...customMeta,
+  };
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta
+          property="og:url"
+          content={`https://yourwebsite.com${router.asPath}`}
+        />
+        <link rel="canonical" href={`https://yourwebsite.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Jared Absher" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
+      <main className="dark:bg-gray-800 w-full">
+        <div>{children}</div>
+      </main>
+    </div>
+  )
+}
